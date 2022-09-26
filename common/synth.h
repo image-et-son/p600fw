c68664d158cbc222305fd9c1ae3e01547458d39a
image-et-son <florian.merz@gmail.com>
2022-09-26 13:42:46 +0200

    prepare for maintenance release v2022.1

--- common/synth.h
+++ common/synth.h
@@ -6,7 +6,7 @@
 #include "hardware.h"
 
 //#define DEBUG
-#define RELEASE "v2022-1"
+#define RELEASE "v2022_1"
 
 #define UART_USE_HW_INTERRUPT // this needs an additional wire that goes from pin C4 to pin E4
 
